a433cbfe600b31aa9418cfa03a77f7b421631e75
fenghang <332393900@qq.com>
2023-02-10 22:30:45 +0800

    feat: Update Index Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { Button } from '@chakra-ui/react'
 import styles from '@/styles/Home.module.css';
+import meetSvg from '@/stitic/meet.svg';
 
 export default function Home() {
   return (
@@ -15,6 +17,19 @@ export default function Home() {
         <header>
           <div className={styles.header}>Meet</div>
         </header>
+        <section>
+          <div className={styles.description}>
+            <div className={styles.descriptionText}>
+              <h2>
+                Read and display data from designs.<br />
+                <strong>Using code.</strong>
+              </h2>
+              <p>Programmatically get data from Figma and four other design tools. All in an open-source Node.js SDK.</p>
+              <Button className={styles.btn} colorScheme='messenger'>Get started</Button>
+            </div>
+            <img src="https://www.gstatic.com/meet/meet_google_one_carousel_promo_icon_0f14bf8fc61484b019827c071ed8111d.svg" alt="" />
+          </div>
+        </section>
         <section>
           <div className={styles.description}>
             <h2>
